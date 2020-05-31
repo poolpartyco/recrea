bd834efbfa397f8c0fe30ba7bbf274b2d86c0179
Ana Maria <abedoy19@eafit.edu.co>
2020-05-31 00:52:51 -0500

    Add product form with axios WIP

--- frontend/pages/add-product.vue
+++ frontend/pages/add-product.vue
@@ -3,15 +3,24 @@
     <div class="display-1">
       Agregar producto
     </div>
-    <v-text-field class="mt-5" label="Nombre"></v-text-field>
+    <v-text-field v-model="name" class="mt-5" label="Nombre"></v-text-field>
     <v-container fluid>
-      <v-textarea autocomplete="Descripción" label="Descripción"></v-textarea>
+      <v-textarea
+        v-model="description"
+        autocomplete="Descripción"
+        label="Descripción"
+      ></v-textarea>
     </v-container>
     <v-col cols="8">
-      <v-text-field label="Precio" value="10.00" prefix="$"></v-text-field>
+      <v-text-field
+        v-model="metricPrice"
+        label="Precio"
+        value="10.00"
+        prefix="$"
+      ></v-text-field>
     </v-col>
     <v-text-field
-      v-model="rows"
+      v-model="quantity"
       class="mx-4"
       label="Cantidad"
       max="50"
@@ -21,8 +30,14 @@
       type="number"
     ></v-text-field>
     <v-text-field class="mt-5" label="Unidades"></v-text-field>
+    <v-text-field
+      v-model="publisher"
+      class="mt-5"
+      label="Publicado por"
+    ></v-text-field>
     <v-file-input
-      v-model="files"
+      v-model="image"
+      class="mt-4"
       color="deep-purple accent-4"
       counter
       label="Adjuntar imagen"
@@ -41,7 +56,7 @@
           v-else-if="index === 2"
           class="overline grey--text text--darken-3 mx-2"
         >
-          +{{ files.length - 2 }} File(s)
+          +{{ image.length - 2 }} File(s)
         </span>
       </template>
     </v-file-input>
@@ -52,9 +67,40 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data: () => ({
-    files: []
-  })
+    image: []
+  }),
+
+  methods: {
+    async validate() {
+      this.$refs.form.validate()
+      const response = await axios.post(
+        'http://f609f50a4c35.ngrok.io/apisec/product/create',
+        {
+          name: this.name,
+          description: this.description,
+          metricPrice: this.metricPrice,
+          image: this.image,
+          quantity: this.quantity,
+          metric: this.metric,
+          tags: this.tags,
+          publisher: this.publisher
+        }
+      )
+      if (response.data.status) {
+        this.reset()
+        this.resetValidation()
+      }
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    }
+  }
 }
 </script>
